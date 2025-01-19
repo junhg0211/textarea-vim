7fdb6164ccaea47a1aef281f0029e10789f39269
Sch <junhg0211@gmail.com>
2025-01-19 00:14:00 +0000

    implement basic movement

---
+++ textarea-vim.js
@@ -0,0 +1,114 @@
+const MODE_NORMAL = "NORMAL";
+
+function getCursorPosition(where) {
+    const selectionEnd = where.selectionEnd;
+    const previousLines = where.value
+        .substring(0, selectionEnd).split(/\n/g);
+    
+    const rows = previousLines.length;
+    const cols = previousLines[rows-1].length;
+
+    return [rows, cols];
+}
+
+function setCursorPosition(where, rows, cols) {
+    const lines = where.value.split(/\n/g);
+
+    // rows
+    rows = Math.min(lines.length, Math.max(0, rows));
+
+    let previousLength = 0;
+    for (let i = 0; i < rows-1; i++) {
+        previousLength += lines[i].length + 1;
+    }
+
+    // cols
+    cols = Math.min(lines[rows-1].length, Math.max(0, cols));
+
+    previousLength += cols;
+
+    // set position
+    where.selectionStart = previousLength;
+    where.selectionEnd = previousLength;
+}
+
+function moveCursor(where, dr, dc) {
+    const [rows, cols] = getCursorPosition(where);
+    setCursorPosition(where, rows + dr, cols + dc)
+}
+
+const COMMAND_RE = /(\d*)([hj-l])/;
+
+const normalCommands = [
+    {
+        key: "k",
+        alias: "",
+        action: (w, r) => moveCursor(w, -r, 0)
+    },
+    {
+        key: "h",
+        alias: "",
+        action: (w, r) => moveCursor(w, 0, -r)
+    },
+    {
+        key: "l",
+        alias: "",
+        action: (w, r) => moveCursor(w, 0, r)
+    },
+    {
+        key: "j",
+        alias: "",
+        action: (w, r) => moveCursor(w, r, 0)
+    },
+]
+
+function processBuffer(buffer, where) {
+    const originalBuffer = buffer;
+    const [command, repeat, key] = buffer.match(COMMAND_RE) || [];
+
+    if (command === undefined) {
+        return "";
+    }
+
+    const repeats = parseInt(repeat) || 1;
+
+    normalCommands.forEach(normalCommand => {
+        if (normalCommand.key !== key) {
+            return;
+        }
+
+        if (normalCommand.alias) {
+            buffer = `${repeat}${normalCommand.alias}${buffer.substring(command.length)}`;
+            return processBuffer(buffer, where);
+        }
+
+        buffer = buffer.substring(command.length);
+        return normalCommand.action(where, repeats);
+    });
+
+    if (buffer === originalBuffer) {
+        return buffer;
+    }
+
+    return processBuffer(buffer, where);
+}
+
+function vimify(target) {
+    let mode = MODE_NORMAL;
+    let buffer = "";
+
+    function press(e) {
+        buffer += String.fromCharCode(e.charCode);
+
+        if (mode === MODE_NORMAL) {
+            e.preventDefault();
+            buffer = processBuffer(buffer, target);
+        }
+    }
+
+    function down(e) {
+    }
+
+    target.addEventListener("keypress", press);
+    target.addEventListener("keydown", down);
+}
